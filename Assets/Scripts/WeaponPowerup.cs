0b395dece0bf241b87984fc0dc7222c5e82ddfca
mvaganov <mvaganov@hotmail.com>
2014-07-17 10:51:17 -0700

    weapon powerups
    * implemented diamond weapon power up
    * added diamond weapon icon

---
+++ Assets/Scripts/WeaponPowerup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponPowerup : MonoBehaviour {
+	public GameObject bulletType;
+	void Start () {
+	}
+	void Update () {
+	}
+	void OnTriggerEnter(Collider col) {
+		Shoot s = col.gameObject.GetComponent<Shoot> ();
+		if(s) {
+			s.bullet = bulletType;
+		}
+	}
+}
---
+++ Assets/Scripts/WeaponTaker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponTaker : MonoBehaviour {
+	public Shoot gun;
+	void Start () {
+	}
+	void Update () {
+	}
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		WeaponPowerup w = hit.collider.gameObject.GetComponent<WeaponPowerup> ();
+		if(w) {
+			gun.bullet = w.bulletType;
+			print(w);
+		}
+	}
+}
